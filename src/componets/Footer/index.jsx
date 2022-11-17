b160254bc0144439f0dff09cf7de12ac51002974
Roberto Oliveira <roberto.oliveira@toughtworks.com>
2022-11-16 23:26:09 -0300

    site desktop ok

--- src/componets/Footer/index.jsx
+++ src/componets/Footer/index.jsx
@@ -8,18 +8,22 @@ const Footer = () => {
     return (
         <div className='footer full-w'>
             <div className='container'>
-            <dir className="logo-row">
-                <div className='logo' src={Logo} alt=""></div>
-                <div className='midias'>
-                    <img src={Insta} alt="instagram logo" />
-                    <img src={Linkedin} alt="linkedin logo" />
+            <div className='box-logo'>
+                <div className="logo-row">
+                    <div className='logo' src={Logo} alt=""></div>
+                    <div className='midias'>
+                        <img src={Insta} alt="instagram logo" />
+                        <img src={Linkedin} alt="linkedin logo" />
+                    </div>
                 </div>
-            </dir>
-            <p>Recife, 2022 <br /> <br /></p>
+                <p>Recife, 2022 <br /> <br /></p>
+            </div>
+            <div className='box-text'>
             <p>Rua Alfândega, 35 – Loja 0401 <br />
                 Bairro do Recife  <br />
                 Shopping Paço Alfândega – Cx.PT 053  <br />
                 Recife – PE | CEP: 50030-030</p>
+            </div>
             </div>
         </div>
     );
